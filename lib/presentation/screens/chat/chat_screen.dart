600fcb59c5150280b5584199536f14dd2b8ba60c
CosmicTiger <luisangelmarcia@gmail.com>
2025-02-26 13:16:27 -0600

    :recycle: refactor: refactor to proper project structure

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:yes_no_app/presentation/chat/widgets/my_message_bubble.dart';
-import 'package:yes_no_app/presentation/chat/widgets/other_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/other_message_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
--- lib/presentation/widgets/chat/other_message_bubble.dart
+++ lib/presentation/widgets/chat/other_message_bubble.dart
@@ -34,8 +34,6 @@ class OtherMessageBubble extends StatelessWidget {
 }
 
 class _ImageBubble extends StatelessWidget {
-  const _ImageBubble({super.key});
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -49,6 +47,17 @@ class _ImageBubble extends StatelessWidget {
           width: size.width * 0.7,
           height: 150,
           fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) {
+              return child;
+            }
+            return Container(
+                width: size.width * 0.7,
+                height: 150,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: const Text('My friend is sending an image...'));
+          },
         ));
   }
 }
